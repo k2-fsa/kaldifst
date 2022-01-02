dc771385ac291f4e956d0b60e6edf98859c52c44
Fangjun Kuang <csukuangfj@gmail.com>
2022-01-02 23:52:48 +0800

    Add fstarcsort.

---
+++ kaldifst/python/csrc/fstarcsort.h
@@ -0,0 +1,16 @@
+// kaldifst/python/csrc/fstarcsort.h
+//
+// Copyright (c)  2021  Xiaomi Corporation (authors: Fangjun Kuang)
+
+#ifndef KALDIFST_PYTHON_CSRC_FSTARCSORT_H_
+#define KALDIFST_PYTHON_CSRC_FSTARCSORT_H_
+
+#include "kaldifst/python/csrc/kaldifst.h"
+
+namespace kaldifst {
+
+void PybindFstArcSort(py::module &m);
+
+}  // namespace kaldifst
+
+#endif  // KALDIFST_PYTHON_CSRC_FSTARCSORT_H_
