86ab5551e74218cf8b11283e1754dd632e0fb928
Fangjun Kuang <csukuangfj@gmail.com>
2022-01-03 19:27:49 +0800

    Add tests for fstarcsort.

--- kaldifst/python/csrc/fstarcsort.cc
+++ kaldifst/python/csrc/fstarcsort.cc
@@ -11,19 +11,22 @@
 namespace kaldifst {
 
 void PybindFstArcSort(py::module &m) {
-  m.def("arcsort", [](fst::MutableFst<fst::StdArc> *in_out,
-                      const std::string &sort_type = "ilabel") {
-    if (sort_type == "ilabel") {
-      fst::ILabelCompare<fst::StdArc> icomp;
-      fst::ArcSort(in_out, icomp);
-    } else if (sort_type == "olabel") {
-      fst::OLabelCompare<fst::StdArc> ocomp;
-      fst::ArcSort(in_out, ocomp);
-    } else {
-      KALDIFST_ERR << "Unsupported sort_type " << sort_type
-                   << ". Expect: ilabel or olabel.";
-    }
-  });
+  m.def(
+      "arcsort",
+      [](fst::MutableFst<fst::StdArc> *in_out,
+         const std::string &sort_type = "ilabel") {
+        if (sort_type == "ilabel") {
+          fst::ILabelCompare<fst::StdArc> icomp;
+          fst::ArcSort(in_out, icomp);
+        } else if (sort_type == "olabel") {
+          fst::OLabelCompare<fst::StdArc> ocomp;
+          fst::ArcSort(in_out, ocomp);
+        } else {
+          KALDIFST_ERR << "Unsupported sort_type " << sort_type
+                       << ". Expect: ilabel or olabel.";
+        }
+      },
+      py::arg("in_out"), py::arg("sort_type") = "ilabel");
 }
 
 }  // namespace kaldifst
---
+++ kaldifst/python/tests/test_arc_sort.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Copyright      2021  Xiaomi Corporation (authors: Fangjun Kuang)
+
+
+import kaldifst
+
+
+def test_arc_sort():
+    s = """
+    0 1 1 4
+    0 1 3 5
+    0 1 2 3
+    0 2 5 2
+    0 2 4 1
+    1 2 2 3
+    1 2 3 1
+    1 2 1 2
+    2
+    """
+    fst = kaldifst.compile(s, acceptor=False)
+
+    assert fst.is_ilabel_sorted is False
+    assert fst.is_olabel_sorted is False
+
+    s1 = kaldifst.draw(fst, acceptor=False, portrait=True)
+    print(s1)  # see https://git.io/JSVap
+
+    kaldifst.arcsort(fst, sort_type="ilabel")
+
+    assert fst.is_ilabel_sorted is True
+    assert fst.is_olabel_sorted is False
+
+    s2 = kaldifst.draw(fst, acceptor=False, portrait=True)
+    print(s2)  # see https://git.io/JSVwK
+
+    kaldifst.arcsort(fst, sort_type="olabel")
+
+    assert fst.is_ilabel_sorted is False
+    assert fst.is_olabel_sorted is True
+
+    s3 = kaldifst.draw(fst, acceptor=False, portrait=True)
+    print(s3)  # see https://git.io/JSVr8
+
+
+def main():
+    test_arc_sort()
+
+
+if __name__ == "__main__":
+    main()
