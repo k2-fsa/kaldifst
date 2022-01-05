d38a9a632ebb5fcdd9909aec5451ba9efa9e6f4a
Fangjun Kuang <csukuangfj@gmail.com>
2022-01-05 11:04:09 +0800

    Add fstreverse.

---
+++ kaldifst/python/csrc/fstreverse.h
@@ -0,0 +1,16 @@
+// kaldifst/python/csrc/fstreverse.h
+//
+// Copyright (c)  2021  Xiaomi Corporation (authors: Fangjun Kuang)
+
+#ifndef KALDIFST_PYTHON_CSRC_FSTREVERSE_H_
+#define KALDIFST_PYTHON_CSRC_FSTREVERSE_H_
+
+#include "kaldifst/python/csrc/kaldifst.h"
+
+namespace kaldifst {
+
+void PybindFstReverse(py::module &m);
+
+}  // namespace kaldifst
+
+#endif  // KALDIFST_PYTHON_CSRC_FSTREVERSE_H_
