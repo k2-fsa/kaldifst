64f65f89bea2ebcc1233bc41091f63eae9afb7a7
Fangjun Kuang <csukuangfj@gmail.com>
2023-01-10 12:23:07 +0800

    small fixes

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -16,33 +16,27 @@
 function(download_pybind11)
   include(FetchContent)
 
-  set(pybind11_URL  "https://github.com/pybind/pybind11/archive/v2.6.0.tar.gz")
-  set(pybind11_HASH "SHA256=90b705137b69ee3b5fc655eaca66d0dc9862ea1759226f7ccd3098425ae69571")
-  
-  # If you don't have access to the Internet, please download the file to your
-  # local drive and replace with the line below (you need to change it accordingly.
-  # I am placing it in /mypath/pybind11-2.6.0.tar.gz, but you can place it
-  # anywhere you like)
-  # set(pybind11_URL  "/mypath/pybind11-2.6.0.tar.gz")
+  set(pybind11_URL  "https://github.com/pybind/pybind11/archive/refs/tags/v2.10.2.tar.gz")
+  set(pybind11_HASH "SHA256=93bd1e625e43e03028a3ea7389bba5d3f9f2596abc074b068e70f4ef9b1314ae")
 
-  set(double_quotes "\"")
-  set(dollar "\$")
-  set(semicolon "\;")
-  if(NOT WIN32)
-    FetchContent_Declare(pybind11
-      URL               ${pybind11_URL}
-      URL_HASH          ${pybind11_HASH}
-    )
-  else()
-    FetchContent_Declare(pybind11
-      URL               ${pybind11_URL}
-      URL_HASH          ${pybind11_HASH}
-    )
+  # If you don't have access to the Internet, please download it to your
+  # local drive and modify the following line according to your needs.
+  if(EXISTS "/star-fj/fangjun/download/github/pybind11-2.10.2.tar.gz")
+    set(pybind11_URL  "file:///star-fj/fangjun/download/github/pybind11-2.10.2.tar.gz")
+  elseif(EXISTS "/Users/fangjun/Downloads/pybind11-2.10.2.tar.gz")
+    set(pybind11_URL  "file:///Users/fangjun/Downloads/pybind11-2.10.2.tar.gz")
+  elseif(EXISTS "/tmp/pybind11-2.10.2.tar.gz")
+    set(pybind11_URL  "file:///tmp/pybind11-2.10.2.tar.gz")
   endif()
+
+  FetchContent_Declare(pybind11
+    URL               ${pybind11_URL}
+    URL_HASH          ${pybind11_HASH}
+  )
 
   FetchContent_GetProperties(pybind11)
   if(NOT pybind11_POPULATED)
-    message(STATUS "Downloading pybind11 ${pybind11_URL}")
+    message(STATUS "Downloading pybind11 from ${pybind11_URL}")
     FetchContent_Populate(pybind11)
   endif()
   message(STATUS "pybind11 is downloaded to ${pybind11_SOURCE_DIR}")
